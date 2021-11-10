8d4a62737c8162c9e81344cddcc353714cc3d838
Jecoro <63196990+Jecoro@users.noreply.github.com>
2021-11-10 18:09:16 +0100

    Update EJ11.py

--- EJ11.py
+++ EJ11.py
@@ -8,10 +8,10 @@ class Circulo: #<-- Clase
   def calcularPeri(self): #<-- 2do Metodo
     pi = 3.14
     return self.diametro*pi
-  radio=float(input("Ingrese el valor del radio: "))
-  diametro=float(input("Ingrese el valor del diámetro: "))
-  cir=Circulo(radio,diametro) # objeto cir llamando a la clase
-  area=cir.calcularArea() # objeto area que llama al 1er metodo
-  perimetro=cir.calcularPeri() #objeto perimetro que llama al 2do metodo
-  print("El area del círculo es: ",area)
-  print("El perimetro del círculo es: ",perimetro)
+radio=float(input("Ingrese el valor del radio: "))
+diametro=float(input("Ingrese el valor del diámetro: "))
+cir=Circulo(radio,diametro) # objeto cir llamando a la clase
+area=cir.calcularArea() # objeto area que llama al 1er metodo
+perimetro=cir.calcularPeri() #objeto perimetro que llama al 2do metodo
+print("El area del círculo es: ",area)
+print("El perimetro del círculo es: ",perimetro)
