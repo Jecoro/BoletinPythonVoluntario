113e9c3077a1a280aed9753fab9f51ed996d7ed1
Jecoro <63196990+Jecoro@users.noreply.github.com>
2021-11-10 17:57:53 +0100

    Update EJ6.py

--- EJ6.py
+++ EJ6.py
@@ -1,23 +1,26 @@
-class Peers(object):
-    
-# Atributo de Clase.
-    list_Numbers = [-25, -10, -7, -3, 2, 4, 8, 10]
+'''Entrada: [-25, -10, -7, -3, 2, 4, 8, 10]
+Salida: [[-10, 2, 8], [-7, -3, 10]] '''
 
-# Constructor.
-    def __init__(self, int_Obj):
-        self.int_Obj = int_Obj
-
-# Método de Instancia.
-    def findPeers(self):
-        for i in range(len(self.list_Numbers)):
-            if (self.list_Numbers[i] + self.list_Numbers[i - 1]) == self.int_Obj:
-                self.i = i
-                self.i_1 = (i-1)
-
-            def __str__(self):
-                    return ("Los Indices que Suman {0} son ({2}, {1}).".format(self.int_Obj, self.i, self.i_1))
-
+class Find(object):
+    def __init__(self, list_Enter):
+        self.listEnter = list_Enter
+        self.listOut = []
+    def set_Zero(self):
+        for i in range(len(self.listEnter)):
+            j = i + 1
+            for j in range(j, len(self.listEnter)):
+                k = j + 1
+                for k in range(k, len(self.listEnter)):
+                    if (self.listEnter[i] + self.listEnter[j] + self.listEnter[k] == 0):
+                        self.listOut.append( [self.listEnter[i], self.listEnter[j], self.listEnter[k]] )
+    def get_Zero(self):
+        return self.listOut
+    def __str__(self):
+        return "{0}".format(self.listOut)
+    sum_Zero = property(get_Zero, set_Zero)
 if __name__ == '__main__':
-    peers = Peers(0)
-    peers.findPeers()
-    print(str(peers))
+    f = Find( [-25,-10,-7,-3,2,4,8,10] )
+    f.set_Zero()
+print("Get: ", f.get_Zero())
+print("Property: ", f.sum_Zero)
+print("STR: ", str(f))
